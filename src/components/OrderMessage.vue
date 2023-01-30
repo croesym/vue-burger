e72400f3c6205e3dfd7b5bc6456f134e2b2ae8ea
Matheus <matheuscroesy@gmail.com>
2023-01-30 12:54:27 -0300

    Criação da Dashbaord de pedidos

---
+++ src/components/OrderMessage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="message-container">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderMessage",
+  props: { message: String },
+};
+</script>
+
+<style scoped>
+.message-container {
+  color: #004085;
+  background-color: #cce5ff;
+  border-color: #b8daff;
+  border: 2px solid #88daff;
+  border-radius: 5px;
+  padding: 10px;
+  max-width: 400px;
+  margin: 30px auto;
+}
+</style>
